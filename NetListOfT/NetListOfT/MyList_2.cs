86202ece68fdc066f99ae1237d7a33cc8ebcb249
Bagiac Andrei <bagaic.andrei@yahoo.ro>
2015-04-27 20:15:11 +0300

    https://github.com/bagiacandrei/NetListOfT.git

---
+++ NetListOfT/NetListOfT/MyList_2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetListOfT
+{
+     class MyList<T>:IEnumerable<T>
+    {
+        private int index;
+        private T[] memory;
+        
+         
+        public MyList()
+        {
+            index = 0;
+            memory = new T[index];
+            
+            
+        }
+        public int Count { get { return index; } }
+        public void Add(T item)
+        {
+            T[] copy = new T[index];
+            copy = this.memory;
+            memory = null;
+            memory=new T[index+1];
+            for (int i = 0; i < index; i++)
+                memory[i] = copy[i];
+            this.memory[index] = item;
+            index++;
+        }
+        public T this[int index]
+        {
+            get { return memory[index]; }
+            set
+            {
+                if (index < 0 || index > Count)
+                    throw new ArgumentOutOfRangeException("!!!");
+            }
+        }
+         public void Clear()
+        {
+            memory = null;
+            index = 0;
+            memory = new T[index];
+
+        }
+         public bool Contains(T item)
+         {
+             for(int i=0;i<index;i++)
+             {    
+                 System.Collections.Generic.EqualityComparer<T> c=System.Collections.Generic.EqualityComparer<T>.Default;//habar nu am cei astaaaa
+                 
+                 if (c.Equals(memory[i],item))
+                     return true;
+                 
+             }
+             return false;
+         }
+         public void RemoveAt(int position)
+         {
+             T[] copy = new T[index];
+             copy = this.memory;
+             memory = null;
+             memory = new T[index - 1];
+             for (int i =0 ; i < position; i++)
+                     memory[i] = copy[i];
+             for (int i = position; i < index - 1; i++)
+                 memory[i] = copy[i + 1];
+                 
+             index--;
+         }
+         public MyList<T> FindAll(T item)
+         {
+             MyList<T> list = new MyList<T>();
+             
+             for (int i = 0; i < index; i++)
+                 {
+                     System.Collections.Generic.EqualityComparer<T> c = System.Collections.Generic.EqualityComparer<T>.Default;
+
+                     if (c.Equals(memory[i], item))
+            
+                     list.Add(memory[i]);
+                 }
+             return list;
+
+         }
+         public IEnumerator<T> GetEnumerator()
+         {
+             return this.GetEnumerator();
+         }
+
+         IEnumerator IEnumerable.GetEnumerator()
+         {
+             return GetEnumerator();
+         }
+        
+    }
+}
--- NetListOfT/NetListOfT/Program.cs
+++ NetListOfT/NetListOfT/Program.cs
@@ -20,53 +20,12 @@ namespace NetListOfT
             Console.WriteLine(list[1].ToString());
                 Console.WriteLine("\nCount :{0}", list.Count);//nr de elemente din lista
                 if (list.Contains(2))
-                    Console.WriteLine("coooor");
+                    Console.WriteLine("It works");
                 list2 = list.FindAll(2);
               Console.WriteLine(list2[1].ToString());
-
-           /*bands.Add(new MyList() { Name = "Led Zeppelin", StudioAlbums = 9, Genre = "Hard Rock", Country = "England" });
-           bands.Add(new MyList() { Name = "Judas Priest", StudioAlbums = 17, Genre = "Heavy Metal", Country = "England" });
-           bands.Add(new MyList() { Name = "Phoenix", StudioAlbums = 10, Genre = "Rock", Country = "Romania" });
-           bands.Add(new MyList() { Name = "Black Sabbath", StudioAlbums = 19, Genre = "Heavy Metal", Country = "England" });
-           bands.Add(new MyList() { Name = "Rammstein", StudioAlbums = 6, Genre = "Industrial Metal", Country = "Germany" });
-            Console.WriteLine();
-            foreach (MyList band in bands)
-            {
-                Console.WriteLine(band);
-            }
-            Console.WriteLine(); 
-            Console.WriteLine("\nCount :{0}", bands.Count());//nr de elemente din lista
-                 
-            Console.WriteLine();
-            
-            var formatie = new MyList() { Name = "Abba", StudioAlbums = 20, Genre = "Soft Rock", Country = "SUA" };
-            bands.Add(formatie);//adaugarea unui element la sfarsitul listei;
-
-            foreach (MyList band in bands)
-            {
-                Console.WriteLine(band);
-            }
-          
-            Console.WriteLine("\nElementul cautat in functie de index:\n{0}", bands[3]);
-            Console.WriteLine("\nContains: {0}", bands.Contains(new MyList { Name = "Abba", StudioAlbums = 20, Genre = "Soft Rock", Country = "SUA" }));
-            var bandsCollection = new MyListCollection(bands);
-            Console.WriteLine("________________");
-            bandsCollection.RemoveAt(2);
-            foreach (MyList band in bands)
-            {
-                Console.WriteLine(band);
-            }
-             bands.Clear();//sterge toata lista
-
-            Console.WriteLine("\nCount :{0}", bands.Count());
-
-            Console.WriteLine("________________");
-            foreach (MyList band in bands)
-                Console.WriteLine(band.Name + " " + band.StudioAlbums + " " + band.Genre + " "+band.Country);
-        */}
-
-        
-            
+            //foreach(MyList<int> x in list)
+               // Console.WriteLine(x.ToString());
+           } 
         
         }
     }
